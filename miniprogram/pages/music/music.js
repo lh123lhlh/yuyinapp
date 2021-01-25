44529b36d40de42ba4ddb598f299cfc09b0ce3a2
lh123lhlh <1472803624@qq.com>
2021-01-25 23:13:39 +0800

    Initial Commit

---
+++ miniprogram/pages/music/music.js
@@ -0,0 +1,75 @@
+// pages/music/music.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [
+      '../../images/image/image1.jpg',
+      '../../images/image/image2.jpg',
+      '../../images/image/image3.jpg'
+    ],
+    indicatorDots: true, // 显示面板指示点
+    autoplay: true,      // 自动切换
+    interval: 3000,      // 切换时间间隔
+    duration: 1000,      // 滑动动画时长
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
